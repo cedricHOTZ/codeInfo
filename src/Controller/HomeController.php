1e1cc9c929ddac4625fb8fb323f2e97af1dac9af
cedricHOTZ <hotz.cedric@gmail.com>
2022-05-10 21:40:46 +0200

    Modal avec formulaire (header)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,11 +2,15 @@
 
 namespace App\Controller;
 
+use PDO;
 use App\Entity\Tag;
 use App\Entity\Posts;
+use App\Form\ContactType;
+use App\Form\PartagePostType;
 use App\Repository\TagRepository;
 use App\Repository\PostsRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -26,7 +30,7 @@ class HomeController extends AbstractController
      * @Route("/", name="home")
      */
 
-    public function index(PostsRepository $postsRepository,TagRepository $tagRepository):Response
+    public function index(PostsRepository $postsRepository,TagRepository $tagRepository, Request $request):Response
      {
     
      // Récupère tout les tags
@@ -35,15 +39,59 @@ class HomeController extends AbstractController
      $listPost = $postsRepository->findByPostPHp('php');
      // Affiche tout les posts
      $posts = $postsRepository->findByExampleField($value = 6);
+     $partage = New Posts();
+     $form = $this->createForm(PartagePostType::class);
+     $form->handleRequest($request);
+     if ($form->isSubmitted() && $form->isValid()) {
+            $partage = $form->getData();   
+            $this->entityManager->persist($partage);
+            $this->entityManager->flush();
+            $this->addFlash('success', 'Votre post a bien été partagé');
+          
+        }
     
         return $this->render('home/index.html.twig', [
             'posts' => $posts,
             'tag' => $listTag,
            'listPost' => $listPost,
+            'form' => $form->createView(),
            
         ]);
        
     }
+ /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact(Request $request):Response{
+      $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contact = $form->getData();
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($contact);
+            $entityManager->flush();
+            $this->addFlash('success', 'Votre message a bien été envoyé');
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('home/contact.html.twig',
+            [
+                'form' => $form->createView(),
+            ]
+            );
+    }
+
+
+
+    /**
+     * @Route("/mentions-légales", name="mentions")
+     */
+    public function mentions(Request $request):Response{
+        
+  
+          return $this->render('home/mentions.html.twig');
+      }
+  
 
   
 }
